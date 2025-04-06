c5513a997251e7b3a7189e27798e22cad08b6c8a
Edward M. Villamaria <edwardvillamaria@gmail.com>
2025-04-06 14:01:29 -0400

    got rid of notification bell and search bar

--- frontend/src/views/Menu.js
+++ frontend/src/views/Menu.js
@@ -4,35 +4,21 @@ import ShelfLyfeLogo from "../assets/images/ShelfLyfeLogo.png";
 import { Link, Outlet, useLocation } from "react-router-dom";
 import { Dialog, Menu, Transition } from "@headlessui/react";
 import {
-  AdjustmentsHorizontalIcon,
   BanknotesIcon,
   Bars3Icon,
-  BellIcon,
-  CalendarIcon,
-  ChartBarIcon,
-  ChartPieIcon,
   ClipboardDocumentListIcon,
   ClipboardDocumentCheckIcon,
   Cog6ToothIcon,
-  DocumentDuplicateIcon,
   FolderIcon,
   HomeIcon,
-  PresentationChartBarIcon,
   PresentationChartLineIcon,
-  ReceiptRefundIcon,
-  RocketLaunchIcon,
-  ShareIcon,
-  StarIcon,
   TableCellsIcon,
-  UsersIcon,
   WrenchIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
 import {
   ChevronDownIcon,
-  MagnifyingGlassIcon,
 } from "@heroicons/react/20/solid";
-import { ScaleIcon } from "@heroicons/react/24/solid";
 
 const navigation = [
   {
@@ -324,38 +310,8 @@ export default function SupplierMenu({ children }) {
               aria-hidden="true"
             />
 
-            <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
-              <form className="relative flex flex-1" action="#" method="GET">
-                <label htmlFor="search-field" className="sr-only">
-                  Search
-                </label>
-                <MagnifyingGlassIcon
-                  className="pointer-events-none absolute inset-y-0 left-0 h-full w-5 text-gray-400"
-                  aria-hidden="true"
-                />
-                <input
-                  id="search-field"
-                  className="block h-full w-full border-0 py-0 pl-8 pr-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm"
-                  placeholder="Search..."
-                  type="search"
-                  name="search"
-                />
-              </form>
+            <div className="flex flex-1 justify-end gap-x-4 self-stretch lg:gap-x-6">
               <div className="flex items-center gap-x-4 lg:gap-x-6">
-                <button
-                  type="button"
-                  className="-m-2.5 p-2.5 text-gray-400 hover:text-gray-500"
-                >
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
-
-                {/* Separator */}
-                <div
-                  className="hidden lg:block lg:h-6 lg:w-px lg:bg-gray-200"
-                  aria-hidden="true"
-                />
-
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative">
                   <Menu.Button className="-m-1.5 flex items-center p-1.5">
@@ -370,7 +326,7 @@ export default function SupplierMenu({ children }) {
                         className="ml-4 text-sm font-semibold leading-6 text-gray-900"
                         aria-hidden="true"
                       >
-                        Tom Cook
+                        Guest
                       </span>
                       <ChevronDownIcon
                         className="ml-2 h-8 w-8 text-gray-400"
